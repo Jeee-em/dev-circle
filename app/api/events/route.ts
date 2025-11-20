56080931f70b969f281ad5b928bddd11bb74a96a
Jeee-em <je.empantaleon@gmail.com>
2025-11-20 20:39:18 +0800

    Implementedimage upload using Cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import Event from '@/database/event.model'
+import { v2 as cloudinary } from "cloudinary"; 
+import { error } from "console";
 
 export async function POST(req: NextRequest) {
     try {
@@ -16,6 +18,25 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ message: "Invalid JSON data format"}, { status: 400})
         }
 
+        const file = formData.get('image') as File;
+
+        if(!file) {
+            return NextResponse.json({ message: "Image file is required"}, {status: 400})
+        }
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevCircle'}, (error, results) => {
+                if(error) return reject(error);
+
+                resolve(results)
+            }).end(buffer)
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({ message: "Event created successfully", event: createdEvent }, { status: 201 })
@@ -25,3 +46,14 @@ export async function POST(req: NextRequest) {
     }
 }
 
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events}, { status: 200})
+    } catch (error) {
+        return NextResponse.json({ message: 'Event fetching failed', error: error}, { status: 500})
+    }
+}
